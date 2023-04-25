c771d8f76dc1dc94b478bfb60c24293a7e0ea92d
Michael Cuffaro <michael@cuffaro.com>
2023-04-25 19:48:44 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,117 @@
+
+# OpenApi IT Rust Client 
+
+This client is used to interact with the API found at [openapi.it](https://openapi.it/)
+
+## Pre-requisites
+
+Before using the OpenApi IT Rust Client, you will need an account at [openapi.it](https://openapi.it/) and an API key to the sandbox and/or production environment
+
+## Installation
+
+You can add the OpenApi IT Rust Client to your project with the following command:
+
+```bash
+cargo add openapiit-cli-rust
+```
+    
+## Usage
+
+```rust
+use std::collections::HashMap;
+
+use openapi_client::{Client, OauthClient};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+struct TokenResponse {
+    token: String,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Initialize the oauthclient
+    let oauth_client =
+        OauthClient::new("<your_username>", "<your_apikey>", true).unwrap();
+
+    // Create a token for a list of scopes
+    let scopes = vec![
+        "GET:test.imprese.openapi.it/advance",
+        "POST:test.postontarget.com/fields/country",
+    ];
+    let ttl = 3600;
+    let result = oauth_client.create_token(scopes, ttl).await?;
+
+    // The string response can be parsed into a custom object
+    let resp: TokenResponse = serde_json::from_str(&result)?;
+    let token = resp.token;
+
+    // Initialize the client
+    let client = Client::new(token.clone()).unwrap();
+
+    // Make a request with Params
+    let mut params = HashMap::new();
+    params.insert("denominazione", "altravia");
+    params.insert("provincia", "RM");
+    params.insert("codice_ateco", "6201");
+
+    let _result = client
+        .request::<serde_json::Value>(
+            "GET",
+            "https://test.imprese.openapi.it/advance",
+            None,
+            Some(params),
+        )
+        .await?;
+
+    // Make a request with a payload, a nested json
+    #[derive(Serialize)]
+    struct Query {
+        country_code: String,
+    }
+
+    #[derive(Serialize)]
+    struct Payload {
+        limit: u64,
+        query: Query,
+    }
+
+    let query = Query {
+        country_code: "IT".to_string(),
+    };
+    let payload = Payload { limit: 0, query };
+
+    let _result = client
+        .request(
+            "POST",
+            "https://test.postontarget.com/fields/country",
+            Some(&payload),
+            None,
+        )
+        .await?;
+
+    // Delete the token
+    let _result = oauth_client.delete_token(token).await?;
+
+    Ok(())
+}
+```
+
+## Contributing
+
+Contributions are always welcome!
+
+See `contributing.md` for ways to get started.
+
+Please adhere to this project's `code of conduct`.
+
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
+
+
+## Authors
+
+- [@maiku1008](https://www.github.com/maiku1008)
+- [@openapi-it](https://github.com/openapi-it)
---
+++ src/lib.rs
@@ -0,0 +1,133 @@
+use base64;
+use http::header::{AUTHORIZATION, CONTENT_TYPE};
+use http::HeaderMap;
+use reqwest::Client as reqwest_client;
+use reqwest::{Method, Response};
+
+use serde::Serialize;
+use std::collections::HashMap;
+use std::str::FromStr;
+
+const OAUTH_BASE_URL: &str = "https://oauth.openapi.it";
+const TEST_OAUTH_BASE_URL: &str = "https://test.oauth.openapi.it";
+
+pub struct OauthClient {
+    client: reqwest_client,
+    url: &'static str,
+}
+
+impl OauthClient {
+    pub fn new(username: &str, apikey: &str, test: bool) -> Result<OauthClient, reqwest::Error> {
+        let url = if test {
+            TEST_OAUTH_BASE_URL
+        } else {
+            OAUTH_BASE_URL
+        };
+        let encoded = base64::encode(format!("{username}:{apikey}"));
+        let auth_header = format!("Basic {encoded}");
+        let mut headers = HeaderMap::new();
+        headers.insert(AUTHORIZATION, auth_header.parse().unwrap());
+        headers.insert(CONTENT_TYPE, "application/json".parse().unwrap());
+
+        let client = reqwest::Client::builder()
+            .default_headers(headers)
+            .build()?;
+        Ok(OauthClient { client, url })
+    }
+
+    pub async fn get_scopes(&self, limit: bool) -> Result<String, reqwest::Error> {
+        let params = [("limit", limit as u8)];
+        let url = format!("{}/scopes", self.url);
+        let response: Response = self.client.get(url).query(&params).send().await?;
+        let json_str: String = response.text().await?;
+        Ok(json_str)
+    }
+
+    pub async fn create_token(
+        &self,
+        scopes: Vec<&'static str>,
+        ttl: u64,
+    ) -> Result<String, reqwest::Error> {
+        #[derive(Serialize)]
+        struct Body {
+            scopes: Vec<&'static str>,
+            ttl: u64,
+        }
+
+        let body = Body { scopes, ttl };
+        let url = format!("{}/token", self.url);
+        let response: Response = self.client.post(url).json(&body).send().await?;
+        let json_str: String = response.text().await?;
+        Ok(json_str)
+    }
+
+    pub async fn get_tokens(&self, scope: &'static str) -> Result<String, reqwest::Error> {
+        let params = [("scope", scope)];
+        let url = format!("{}/token", self.url);
+        let response: Response = self.client.get(url).query(&params).send().await?;
+        let json_str: String = response.text().await?;
+        Ok(json_str)
+    }
+
+    pub async fn delete_token(&self, id: String) -> Result<String, reqwest::Error> {
+        let url = format!("{}/token/{}", self.url, id);
+        let response: Response = self.client.delete(url).send().await?;
+        let json_str: String = response.text().await?;
+        Ok(json_str)
+    }
+
+    pub async fn get_counters(
+        &self,
+        period: &'static str,
+        date: &'static str,
+    ) -> Result<String, reqwest::Error> {
+        let url = format!("{}/counters/{}/{}", self.url, period, date);
+        let response: Response = self.client.get(url).send().await?;
+        let json_str: String = response.text().await?;
+        Ok(json_str)
+    }
+}
+
+pub struct Client {
+    client: reqwest_client,
+}
+
+impl Client {
+    pub fn new(token: String) -> Result<Client, reqwest::Error> {
+        let auth_header = format!("Bearer {token}");
+        let mut headers = HeaderMap::new();
+        headers.insert(AUTHORIZATION, auth_header.parse().unwrap());
+        headers.insert(CONTENT_TYPE, "application/json".parse().unwrap());
+
+        let client = reqwest::Client::builder()
+            .default_headers(headers)
+            .build()?;
+        Ok(Client { client })
+    }
+
+    pub async fn request<T>(
+        &self,
+        method: &str,
+        url: &str,
+        payload: Option<&T>,
+        params: Option<HashMap<&str, &str>>,
+    ) -> Result<String, reqwest::Error>
+    where
+        T: Serialize,
+    {
+        let url = format!("{}", url);
+        let mut request = self.client.request(Method::from_str(method).unwrap(), url);
+
+        if let Some(payload) = payload {
+            request = request.json(payload);
+        }
+
+        if let Some(params) = params {
+            request = request.query(&params);
+        }
+
+        let response: Response = request.send().await?;
+        let json_str: String = response.text().await?;
+        Ok(json_str)
+    }
+}
